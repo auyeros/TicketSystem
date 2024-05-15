eb1d62ad478b0c53b898a4b0f9043d8b0cb82aff
auyeros <auyerosignacio@gmail.com>
2024-05-15 19:24:41 -0300

    movi la carpeta de netlify

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -38,7 +38,7 @@ export default function TicketForm() {
   };
 
   const sendEmailWithQRCode = async (to, qrCodeBase64, qrData) => {
-    const response = await fetch('/.netlify/functions/send-email', {
+    const response = await fetch('/ticketing-system/netlify/functions/send-email.js', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
